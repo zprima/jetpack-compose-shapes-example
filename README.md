e852fa73e73660aabc861a48c3c4c3e303f11871
Primoz <sg.prima@gmail.com>
2020-12-12 17:35:26 +0100

    Custom Shapes example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jetpack-compose-shapes-example
 Jetpack Compose custom shapes example
+   
+<img src="pic.png" height="400px" />
---
+++ app/src/main/java/com/example/customshapes/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.example.customshapes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.GenericShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.platform.AmbientDensity
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
+import com.example.customshapes.ui.CustomShapesTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomShapesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    App()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App(){
+    Column(modifier = Modifier.fillMaxSize(1f),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        MyCircle()
+        Spacer(modifier = Modifier.size(20.dp))
+        MyRect()
+        Spacer(modifier = Modifier.size(20.dp))
+        MyTriangle()
+        Spacer(modifier = Modifier.size(20.dp))
+        MyStarTrek()
+    }
+}
+
+@Composable
+fun MyCircle(){
+    Canvas(modifier = Modifier.size(100.dp), onDraw = {
+        drawCircle(color = Color.Red)
+    })
+}
+
+@Composable
+fun MyRect(){
+    val density = AmbientDensity.current
+
+    val w = remember { 100f * density.density }
+    val h = remember { 33f * density.density }
+
+    Canvas(modifier = Modifier.size(100.dp), onDraw = {
+        drawRect(color = Color.Black, size = Size(w, h), topLeft = Offset.Zero)
+        drawRect(color = Color.Red, size = Size(w, h), topLeft = Offset(0f, h))
+        drawRect(color = Color.Yellow, size = Size(w, h), topLeft = Offset(0f, h * 2))
+    })
+}
+
+@Composable
+fun MyTriangle(){
+    val density = AmbientDensity.current.density
+
+    val path = Path().apply {
+        moveTo(50f * density, 0f)
+        lineTo(100f * density, 100f * density)
+        lineTo(0f, 100f * density)
+        close()
+    }
+
+    Canvas(modifier = Modifier.size(100.dp), onDraw = {
+        drawPath(path, Color.Blue)
+    })
+}
+
+
+@Composable
+fun MyStarTrek(){
+    val myStarTrekShape = GenericShape { size ->
+        moveTo(size.width / 2f, 0f)
+        lineTo(size.width, size.height)
+        quadraticBezierTo(
+            size.width * 0.6f,
+            size.height * 0.4f,
+            0f,
+            size.height
+        )
+        close()
+    }
+
+    Surface(
+        shape = myStarTrekShape,
+        color = Color.Yellow,
+        border = BorderStroke(3.dp, Color.Black),
+        modifier = Modifier.size(100.dp)
+    ) { }
+}
